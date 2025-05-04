004a4a7d5d6ba1800231097a71a7968d168b08c8
Ясонов Иван <64076990+Roherim@users.noreply.github.com>
2025-05-04 19:04:51 +0300

    work started

--- config.py
+++ config.py
@@ -1,4 +1,33 @@
 
+
+
 SERVER_DOMAIN = 'https://test_url.com'
 
 GET_RESERVATION_URL = f'{SERVER_DOMAIN}/api/get_reservations'
+
+ADD_RESERVATION_URL = f'{SERVER_DOMAIN}/api/add_reservation'
+
+ADDRESSES = [
+    'улица Пушкина, 12',
+    'улица Ленина, 5',
+    'улица Ясонова, 7',
+    'Ордынский проспект, 666'
+]
+
+FREE_DATES=[
+    '2025-03-12',
+    '2025-03-13',
+    '2025-03-14',
+    '2025-03-15',
+    '2025-03-16',
+]
+FREE_TIME = [
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00',
+]
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ DEBUG = True
 def start(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton("Получить бронирования", callback_data='get_reserws'))
+    markup.add(types.InlineKeyboardButton("Забронировать", callback_data='create_reserw'))
     bot.send_message(message.chat.id, "Выберите действие", reply_markup=markup)
    
 
@@ -22,19 +23,43 @@ def start(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == 'get_reserws':
+        new_reserve = {'chat_id': call.message.chat.id}
         data = get_reserws(call.message.chat.id)
-        sending_data = ',\n'.join([f'время: {i["time"]}, имя: {i["name"]}' for i in data])
+        sending_data = ',\n'.join([f'столик в ресторане по адресу {i['address']}, на: {datetime.datetime.strftime(i["time"], "%d.%m.%Y %H:%M")}, имя: {i["name"]}' for i in data['res']])
         bot.send_message(call.message.chat.id, f'Ваши бронирования: {sending_data}')
-        
+    elif call.data =='create_reserw':
+        markup = types.InlineKeyboardMarkup()
+        for address in config.ADDRESSES:
+            markup.add(types.InlineKeyboardButton(address, callback_data=address))
+        bot.send_message(call.message.chat.id, 'Выберите ресторан', reply_markup=markup)
+    elif call.data in config.ADDRESSES:
 
+
+def change_data(data):
+    return data
 
 def get_reserws(user_id):
     if DEBUG:
-        return [{'time': datetime.datetime.now(), 'name': 'name1'}, {'time': datetime.datetime.now()- datetime.timedelta(days=1), 'name': 'name2'}]
+        return {'err':None, 'status_code':200, 'res':[{'time': datetime.datetime.now()+ datetime.timedelta(days=3),
+                                                        'name': 'Иван',
+                                                          'address': 'ул. Ленина, д. 1'},
+                                                       {'time': datetime.datetime.now()+ datetime.timedelta(days=21),
+                                                         'name': 'Иван',
+                                                           'address': 'ул. Ленина, д. 1'}]}
     response = requests.get(config.GET_RESERVATION_URL, params={'user_id': user_id})
     if response.status_code == 200:
         return response.json()
     return None
+
+def add_reserws(user_id, address,date, time):
+    if DEBUG:
+        return {'err':None, 'status_code':200}
+    response = requests.post(config.ADD_RESERVATION_URL, json={'user_id': user_id, 'address': address, 'date': date, 'time': time})
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+
 
 
 
